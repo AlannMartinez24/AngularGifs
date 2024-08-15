6e75489fed0d75f176aa8e3b6e2898150a15d2bf
AlannMartinez <alannmtz.soporte@gmail.com>
2024-08-15 15:00:32 -0600

    Ultima version Aplicacion Gifs

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { SharedModule } from '../shared/shared.module';
+
+import { CardListComponent } from './components/card-list/card-list/card-list.component';
+import { GifsCardComponent } from './components/gifs-card/gifs-card.component';
 import { HomePageComponent } from './pages/home/home-page.component';
 import { SearchBoxComponent } from './components/search-box/search-box/search-box.component';
-import { CardListComponent } from './components/card-list/card-list/card-list.component';
-
-
 
 
 @NgModule({
@@ -12,9 +14,11 @@ import { CardListComponent } from './components/card-list/card-list/card-list.co
     HomePageComponent,
     SearchBoxComponent,
     CardListComponent,
+    GifsCardComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   exports: [
     HomePageComponent,
